b05fd7da9ee21b6ca2c7912682aacfb79a248888
Idris <idris@amartha.com>
2021-07-29 22:57:14 +0700

    custom remote data source

---
+++ Today/DetailViewDataSource.swift
@@ -0,0 +1,80 @@
+//
+//  DetailViewDataSource.swift
+//  Today
+//
+//  Created by Idris on 29/07/2021.
+//
+
+import UIKit
+
+class ReminderDetailViewDataSource: NSObject {
+    enum ReminderRow: Int, CaseIterable {
+        case title
+        case date
+        case time
+        case notes
+        
+        static let timeFormatter: DateFormatter = {
+
+            let formatter = DateFormatter()
+
+            formatter.dateStyle = .none
+
+            formatter.timeStyle = .short
+
+            return formatter
+
+        }()
+        
+        func displayText(for reminder: Reminder?) -> String? {
+            switch self {
+            case .title:
+                return reminder?.title
+            case .date, .time:
+                guard let date = reminder?.dueDate else { return nil }
+                return Self.timeFormatter.string(from: date)
+            case .notes:
+                return reminder?.notes
+            }
+        }
+        
+        var cellImage: UIImage? {
+            switch self {
+            case .title:
+                return nil
+            case .date:
+                return UIImage(systemName: "calendar.circle")
+            case .time:
+                return UIImage(systemName: "clock")
+            case .notes:
+                return UIImage(systemName: "square.and.pencil")
+            }
+        }
+    }
+    
+    private var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        super.init()
+    }
+}
+
+extension ReminderDetailViewDataSource: UITableViewDataSource {
+    static let reminderDetailCellIdentifier = "ReminderDetailCell"
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ReminderRow.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderDetailCellIdentifier, for: indexPath)
+        let row = ReminderRow(rawValue: indexPath.row)
+        
+        cell.textLabel?.text = row?.displayText(for: reminder)
+        cell.imageView?.image = row?.cellImage
+        
+        return cell
+    }
+}
